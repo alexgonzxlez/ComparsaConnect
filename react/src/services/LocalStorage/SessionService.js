c816f9f45e311a9a838d4a4d400476136e4d3eb7
alexvilanova <algoar@fp.insjoaquimmir.cat>
2024-04-23 17:33:43 +0200

    services

---
+++ react/src/services/LocalStorage/SessionService.js
@@ -0,0 +1,23 @@
+export default class SessionService {
+    sessionKey;
+  
+    constructor(key = "react_session") {
+      this.sessionKey = key;
+    }
+  
+    createSession(data) {
+      localStorage.setItem(this.sessionKey, JSON.stringify(data));
+    }
+  
+    destroySession() {
+      localStorage.removeItem(this.sessionKey);
+    }
+  
+    getSessionData() {
+      return JSON.parse(localStorage.getItem(this.sessionKey)) || {};
+    }
+  
+    isAuthenticated() {
+      return !!this.getSessionData();
+    }
+  }

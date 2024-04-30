400e81e8266d49fd2ae42cbfb5b6e96cc54140bc
alexvilanova <algoar@fp.insjoaquimmir.cat>
2024-04-30 19:37:44 +0200

    User factory

--- api/database/seeders/DatabaseSeeder.php
+++ api/database/seeders/DatabaseSeeder.php
@@ -18,6 +18,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             GenderSeeder::class,
             BanderaSeeder::class,
+            UsersSeeder::class,
+
         ]);
     }
 }

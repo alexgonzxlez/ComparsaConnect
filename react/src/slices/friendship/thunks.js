ccf08673b266cc1372284a1eaebb3992cf6f79ce
CristianMartinezGuerrero <crmagu@fp.insjoaquimmir.cat>
2024-04-30 20:12:32 +0200

    friendrequest

--- react/src/slices/friendship/thunks.js
+++ react/src/slices/friendship/thunks.js
@@ -1,12 +1,12 @@
-import { setSearchdata, startLoading } from "./friendshipSlice";
+import { setSearchdata, startLoading, updateSearchData } from "./friendshipSlice";
 
 export const searchUsers = (filter) => {
     return async (dispatch, getState) => {
         dispatch(startLoading())
         const searchParams = new URLSearchParams();
-        searchParams.append("query", filter); 
+        searchParams.append("query", filter);
         const { token } = getState().auth;
-        
+
         try {
             const data = await fetch(process.env.API_URL + "users/search?" + searchParams.toString(), {
                 headers: {
@@ -24,6 +24,66 @@ export const searchUsers = (filter) => {
         } catch (error) {
             // dispatch(setError(error));
             console.error(error)
+        }
+    };
+};
+
+export const sendFriendRequest = (id) => {
+    return async (dispatch, getState) => {
+        const { token } = getState().auth;
+        try {
+            const data = await fetch(process.env.API_URL + "send-friend/" + id, {
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`
+                },
+                method: "POST",
+            });
+            const resposta = await data.json();
+            if (resposta.success) {
+                const searchdata = getState().friendship.searchdata;
+                const updatedSearchData = searchdata.map(user => {
+                    if (user.id === id) {
+                        return { ...user, friend_status: 'sended' };
+                    }
+                    return user;
+                });
+                dispatch(setSearchdata(updatedSearchData));
+            }
+        } catch (error) {
+            console.error(error);
+            // Maneja el error aquí
+        }
+    };
+};
+
+export const cancelFriendRequest = (id) => {
+    return async (dispatch, getState) => {
+        const { token } = getState().auth;
+        try {
+            const data = await fetch(process.env.API_URL + "cancel-friend/" + id, {
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`
+                },
+                method: "DELETE",
+            });
+            const resposta = await data.json();
+            if (resposta.success) {
+                const searchdata = getState().friendship.searchdata;
+                const updatedSearchData = searchdata.map(user => {
+                    if (user.id === id) {
+                        return { ...user, friend_status: null };
+                    }
+                    return user;
+                });
+                dispatch(setSearchdata(updatedSearchData));
+            }
+        } catch (error) {
+            console.error(error);
+            // Maneja el error aquí
         }
     };
 };

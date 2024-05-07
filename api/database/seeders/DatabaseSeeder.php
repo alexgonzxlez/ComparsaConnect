4aa663df334ddb12c311a435418791306e84d1ea
alexvilanova <algoar@fp.insjoaquimmir.cat>
2024-05-07 15:54:35 +0200

    Delete account

--- api/database/seeders/DatabaseSeeder.php
+++ api/database/seeders/DatabaseSeeder.php
@@ -18,7 +18,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             GenderSeeder::class,
             BanderaSeeder::class,
-            UsersSeeder::class,
+            // UsersSeeder::class,
 
         ]);
     }
